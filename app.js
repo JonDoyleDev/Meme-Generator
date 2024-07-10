160f532192ba6039e379ea9e3e3ccd56bd64c229
Jon Doyle <120639770+JonDoyleDev@users.noreply.github.com>
2024-07-10 14:57:08 -0600

    Add files via upload

---
+++ app.js
@@ -0,0 +1,25 @@
+const genbtn = document.querySelector(
+    ".meme-generator .genbtn"
+  );
+  const memeImage = document.querySelector(".meme-generator img");
+  const memeTitle = document.querySelector(".meme-generator .meme-title");
+  const memeAuthor = document.querySelector(".meme-generator .meme-author");
+  
+  const updateDetails = (url, title, author) => {
+    memeImage.setAttribute("src", url);
+    memeTitle.innerHTML = title;
+    memeAuthor.innerHTML = `Meme by: ${author} | <a href="https://github.com/JonDoyleDev" target="_blank">Github: JonDoyleDev</a>`;
+
+  };
+  
+  const generateMeme = () => {
+    fetch("https://meme-api.com/gimme/wholesomememes")
+      .then((response) => response.json())
+      .then((data) => {
+        updateDetails(data.url, data.title, data.author);
+      });
+  };
+  
+  genbtn.addEventListener("click", generateMeme);
+  
+  generateMeme();
